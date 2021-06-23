a71ed2df97e7bfa0e979a4e7cb6f65ff2ca9bbe2
Seungyeon <dltmddus95@gmail.com>
2021-06-24 08:54:49 +0900

    0624_test_commit

--- LSY/kakaologin/app/src/main/java/com/example/kakaologin/common/CommonInfo.java
+++
@@ -1,8 +0,0 @@
-package com.example.kakaologin.common;
-
-public class CommonInfo {
-
-    public static String hostIP = "192.168.2.9";
-    public static String hostRootAddr = "http://" + hostIP + ":8080/phonebook/";
-
-}
